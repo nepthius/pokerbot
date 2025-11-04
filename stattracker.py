5a064d79e48c11ac2b7699a96c45611b9375f65b
nepthius <anishdshekar@gmail.com>
2025-11-04 17:37:02 -0500

    add player class with the game class

--- stattracker.py
+++ stattracker.py
@@ -2,10 +2,10 @@ from pokerhelpers import deck
 import random
 
 class Game:
-    def __init__(self, starting_amt, number_players):
-        self.number_players = number_players
+    def __init__(self, starting_amt=2000, players=[]):
+        self.players = players
         self.starting_amt = starting_amt
-        self.turn = 0
+        self.round = 0
         self.deck = deck
 
 
@@ -13,27 +13,47 @@ class Game:
         self.deck = deck
 
     def deal(self):
-        possible_vals = self.deck.keys()
-        card1 = random.choice(possible_vals)
-        suit1 = random.choice(self.deck[possible_vals])
-        self.deck[possible_vals].remove(suit1)
-        if self.deck[possible_vals] == []:
-            del self.deck[possible_vals]
+        def pickCard():
+            possible_vals = list(self.deck.keys())
+            card1 = random.choice(possible_vals)
+            suit1 = random.choice(self.deck[card1])
+            self.deck[card1].remove(suit1)
+            if self.deck[card1] == []:
+                del self.deck[card1]
+            return [card1, suit1]
         
-        card2 = random.choice(possible_vals)
-        suit2 = random.choice(self.deck[possible_vals])
-        self.deck[possible_vals].remove(suit1)
-        if self.deck[possible_vals] == []:
-            del self.deck[possible_vals]
+        first = pickCard()
+        second = pickCard()
 
-        return [[card1, suit1], [card2, suit2]]
+        return [first, second]
+
+    def play(self):
+        if self.round == 0:
+            for player in self.number_players:
+                player.assignHand(self.deal())
+        
+
+        
+
+
+
+
 
 class Player:
     def __init__(self, starting_amt):
         self.pot = starting_amt
         self.hand = []
+    
+    def assignHand(self, cards):
+        self.hand = cards
+
 
 if __name__ == "__main__":
     print("Starting game...")
     players = int(input("Please enter the number of players: "))
     starting_amt = int(input("Please enter the starting amount: "))
+
+    game = Game(starting_amt, players)
+    
+    
+    print(game.deal())
